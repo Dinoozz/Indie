33577a6de7676272cbc00af9fda6d0946df0d0f8
Camille Lecoq <camille.lecoq@epitech.eu>
2020-06-11 01:33:49 +0200

    ca marche

--- Indie/main.cpp
+++ Indie/main.cpp
@@ -2,7 +2,7 @@
 #include "event.h"
 
 
-std::vector <Node> map_gen(const std::string map2D, Mesh crate_mesh, game_t* game)
+std::vector <Node> map_gen3D(char **map2D, Mesh crate_mesh, game_t* game)
 {
     int x = -15;
     int y = 2;
@@ -10,27 +10,32 @@ std::vector <Node> map_gen(const std::string map2D, Mesh crate_mesh, game_t* gam
     int i = 0;
     std::vector <Node> destructibleList;
 
-    while (map2D[i]) {
-        if (map2D[i] == '#') {
-            Node tmp(crate_mesh, "../media/white_marble_03_4k_baseColor.tga", game);
-            tmp.getnode()->setPosition(vector3df(x, y, z));
-            x += 2;
+    for (int j = 0; map2D[j]; j++) {
+        i = 0;
+        while (map2D[j][i]) {
+            if (map2D[j][i] == '#') {
+                Node tmp(crate_mesh, "../media/white_marble_03_4k_baseColor.tga", game);
+                tmp.getnode()->setPosition(vector3df(x, y, z));
+                destructibleList.emplace_back(tmp);
+                x += 2;
+            }
+            else if (map2D[j][i] == '*') {
+                Node tmp(crate_mesh, "../media/crate_1.jpg", game);
+                tmp.getnode()->setPosition(vector3df(x, y, z));
+                destructibleList.emplace_back(tmp);
+                x += 2;
+            }
+            else if (map2D[j][i] == ' ') {
+                x += 2;
+            }
+            else if (map2D[j][i] == '\n') {
+                z += 2;
+                x = -15;
+            }
+            i++;
         }
-        else if (map2D[i] == '*') {
-            Node tmp(crate_mesh, "../media/crate_1.jpg", game);
-            tmp.getnode()->setPosition(vector3df(x, y, z));
-            destructibleList.emplace_back(tmp);
-            x += 2;
-        }
-        else if (map2D[i] == ' ') {
-            x += 2;
-        }
-        else if (map2D[i] == '\n') {
-            z += 2;
-            x = -15;
-        }
-        i++;
     }
+
     return destructibleList;
 }
 
@@ -50,7 +55,6 @@ bool move_player(const f32 frameDeltaTime, MyEventReceiver receiver, Node *bombe
         droit = 0.6;
         is_running = true;
     }
-
     if (receiver.IsKeyDown(irr::KEY_KEY_Z)) {
         player_position.Z += MOVEMENT_SPEED * frameDeltaTime * coté;
         bomberman->getnode()->setRotation(vector3df(0, 180, 0));
@@ -84,10 +88,18 @@ void place_bomb(Mesh bomb_mesh, game_t game, Node bomberman, MyEventReceiver rec
 {
     if (receiver.IsKeyDown(irr::KEY_KEY_E)) {
         Node bomb(bomb_mesh, "../media/Albedo2.png", &game);
-        bomb.getnode()->setScale(vector3df(2, 2, 2));
+        bomb.getnode()->setScale(vector3df(4, 4, 4));
         vector3df position = bomberman.getnode()->getPosition();
         bomb.getnode()->setPosition(vector3df(int(position.X), position.Y + 0.5, int(position.Z)));
     }
+}
+
+std::vector <Node> reloadMap(std::vector <Node> blocks, Mesh crate_mesh, game_t* game)
+{
+
+    for (int i = 0; i < blocks.size(); i++)
+        blocks.at(i).getnode()->remove();
+    return (map_gen3D(map_gen(15), crate_mesh, game));
 }
 
 int main(void)
@@ -107,23 +119,24 @@ int main(void)
     Mesh crate_mesh("../media/Crate1.obj", &game);
     Mesh bomberman_mesh("../media/Bomberman.md3", &game);
     Mesh bomb_mesh("../media/bomb.obj", &game);
-    Node ground(crate_mesh, "../media/white_marble_03_4k_baseColor.tga", &game);
+    Node ground(crate_mesh, "../media/grass.jpg", &game);
     Node bomberman(bomberman_mesh, "../media/WhiteBombermanTextures.png", &game);
     bomberman.getnode()->setPosition(core::vector3df(0, 1, 0));
 
-    ground.getnode()->setScale(core::vector3df(15, 1, 15));
+    ground.getnode()->setScale(core::vector3df(17, 1, 17));
+    ground.getnode()->setPosition(core::vector3df(1, 0, 1));
+
 
     //MAP GEN
    
-    std::vector <Node> destructibleList = map_gen("###############\n#  *********  #\n# #*#*#*#*#*# #\n#*************#\n#*#*#*#*#*#*#*#\n#*************#\n#*#*#*#*#*#*#*#\n#*************#\n#*#*#*#*#*#*#*#\n#*************#\n# #*#*#*#*#*# #\n#  *********  #\n###############", crate_mesh, &game);
+    std::vector <Node> destructibleList = map_gen3D(map_gen(15), crate_mesh, &game);
 
 
     bomberman.getnode()->setFrameLoop(27, 64);
-    ground.getnode()->setScale(core::vector3df(10, 1, 10));
 
 
-    ICameraSceneNode *camera = game.smgr->addCameraSceneNode(0, vector3df(0, 10, -10), vector3df(0, 0, 0));//
-    //game.smgr->addCameraSceneNodeFPS(0, 100.0f, 0.02f);
+    //ICameraSceneNode *camera = game.smgr->addCameraSceneNode(0, vector3df(0, 10, -10), vector3df(0, 0, 0));//
+    game.smgr->addCameraSceneNodeFPS(0, 100.0f, 0.02f);
     game.device->getCursorControl()->setVisible(false);
     u32 then = game.device->getTimer()->getTime();
 
@@ -139,6 +152,9 @@ int main(void)
 
         is_running = player_movement(frameDeltaTime, receiver, &bomberman, is_running);
         place_bomb(bomb_mesh, game, bomberman, receiver);
+        if (receiver.IsKeyDown(irr::KEY_KEY_L)) {
+            destructibleList = reloadMap(destructibleList, crate_mesh, &game);
+        }
 
         game.smgr->drawAll();
         game.guienv->drawAll();
